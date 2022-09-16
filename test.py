b05822fa64e13cab172263866bf6b4fb78fe19a0
jslee <ethanleekk@gmail.com>
2022-09-16 13:46:44 +0900

    test.py changed

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 import numpy as np
+import pandas as pd
+import open3d as o3d
 
 
-print(f'test file...')
+if __name__ == "__main__":
+
+    
+    print(f"Load a ply point cloud. print it, and render it")
+    ply_point_cloud = o3d.data.PLYPointCloud()
+    pcd = o3d.io.read_point_cloud(ply_point_cloud.path)
+    print(pcd)
+    print(np.asarray(pcd.points))
+    o3d.visualization.draw_geometries([pcd])
+    
+    
+    print(f"test file...")
